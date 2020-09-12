3b17c2def6055302ba2a9b903d8609d382baf3b6
lisadebona <annalisa.quiamco@gmail.com>
2020-09-12 02:32:27 -0600

    Added theme files

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;669;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<script defer src="<?php bloginfo( 'template_url' ); ?>/assets/svg-with-js/js/fontawesome-all.js"></script>
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="mobile-menu-backdrop"></div>
+<div id="mobile-navigation">
+	<a href="#" id="closeMobileMenu"><span class="sr">Close</span></a>
+	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-mobile-menu', 'menu_class'=>'mobile-menu','container'=>false ) ); ?>
+</div>
+
+<div id="page" class="site cf">
+	<a class="skip-link sr" href="#content"><?php esc_html_e( 'Skip to content', 'bellaworks' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="wrapper">
+			<div class="flexwrap">
+				<?php if( get_custom_logo() ) { ?>
+	          <div class="logo">
+	          	<?php the_custom_logo(); ?>
+	          </div>
+	      <?php } else { ?>
+	          <h1 class="logo">
+	            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+	          </h1>
+	      <?php } ?>
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'MENU', 'bellaworks' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu','link_before'=>'<span>','link_after'=>'</span>','container'=>false) ); ?>
+				</nav><!-- #site-navigation -->
+			</div>
+	</div><!-- wrapper -->
+	</header><!-- #masthead -->
+
+	<?php get_template_part("parts/banner"); ?>
+
+	<div id="content" class="site-content wrapper">
---
+++ parts/banner.php
@@ -0,0 +1,93 @@
+<?php
+$homeBanner = ( is_front_page() ) ? 'homepage':'subpage';
+$slides = get_field("hero_image");
+$count = 0;
+if( isset($slides['url']) && $slides['url'] ) {
+	$count = 0;
+} else {
+	$count = ($slides) ? count($slides) : 0; 
+}
+$slidesId = ($count>1) ? 'slideshow':'static-banner';
+$placeholder = THEMEURI . 'images/rectangle.png';
+
+if( is_front_page() ) {
+	$tagline = get_field("tagline");
+	if( $slides ) {  ?>
+		<div id="<?php echo $slidesId ?>" class="swiper-container banner-wrap fw homepage">
+			<div class="swiper-wrapper">
+
+				<?php if ( isset($slides['url']) && $slides['url'] ) { ?>
+
+					<div class="swiper-slide slideItem" style="background-image:url('<?php echo $slides['url'] ?>');">
+						<?php if ($tagline) { ?>
+						<div class="slideCaption tagline">
+							<div class="slideInside animated">
+    						<div class="slideMid fadeInDown wow">
+									<div class="slideTitle"><?php echo $tagline ?></div>
+								</div>
+							</div>
+						</div>
+						<?php } ?>
+						<img src="<?php echo $placeholder ?>" alt="" aria-hidden="true" class="placeholder"/>
+					</div>
+
+				<?php } else { ?>
+
+					<?php foreach ($slides as $img) { 
+							$title = $img['title'];
+							$caption = $img['caption'];
+						?>
+	    				<div class="swiper-slide slideItem" style="background-image:url('<?php echo $img['url'] ?>');">
+	    					<?php if ($caption) { ?>
+	    					<div class="slideCaption">
+		    					<div class="slideInside">
+		    						<div class="slideMid animated">
+			    						<?php if ($title) { ?>
+			    						<h2 class="slideTitle"><?php echo $title; ?></h2>
+			    						<?php } ?>
+			    						<?php if ($caption) { ?>
+			    						<div class="slideText"><?php echo $caption; ?></div>	
+			    						<?php } ?>
+		    						</div>
+	    						</div>
+	    					</div>
+	    					<?php } ?>
+	    					<img src="<?php echo $placeholder ?>" alt="" aria-hidden="true" class="placeholder"/>
+	    				</div>
+	    			<?php } ?>
+
+				<?php } ?>
+
+			</div>
+
+			<?php if ($count>1) { ?>
+			    <div class="swiper-pagination"></div>
+			    <div class="swiper-button-next"></div>
+			    <div class="swiper-button-prev"></div>
+			<?php } ?>
+		</div>
+		<div class="scrollDownDiv" style="display:none;">
+			<div class="wrapper"><a href="#" id="scrolldown" class="fadeInDown wow" data-wow-delay=".5s"><span class="sr">scroll down</span></a></div>
+		</div>
+	<?php } ?>
+
+<?php } else { ?>
+
+	<?php  
+	$post_type = get_post_type();
+	$page_title = get_the_title();
+	?>
+	
+	<?php if( $slides ) {  ?>
+	<div id="static-banner" class="banner-wrap fw subpage">
+		<div class="banner-image cf fadeIn animated" style="background-image:url('<?php echo $slides['url']; ?>');">
+			<div class="wrapper">
+				<div class="caption">
+					<h1 class="page-title fadeInRight wow"><?php echo $page_title ?></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+<?php } ?>
