1501d16682aff0bb44a0f062de267282b025c002
lisadebona <annalisa.quiamco@gmail.com>
2020-09-15 03:00:36 -0600

    Styling generic page

--- page.php
+++ page.php
@@ -14,25 +14,19 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area-full default-template">
 		<main id="main" class="site-main" role="main">
+			<div class="wrapper">
+				<?php
+				while ( have_posts() ) : the_post();
 
-			<?php
-			while ( have_posts() ) : the_post();
+					get_template_part( 'parts/content', 'page' );
 
-				get_template_part( 'parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
+				endwhile; // End of the loop.
+				?>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
